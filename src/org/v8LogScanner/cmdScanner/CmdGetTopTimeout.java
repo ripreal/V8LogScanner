032bb23524d04111c89b9e7e12c8430255340158
Кудаков Роман Иванович <roman.kudakov@avilon-nymm.ru>
2017-03-13 09:07:38 +0300

    fancy formatting

--- src/org/v8LogScanner/cmdScanner/CmdGetTopTimeout.java
+++ src/org/v8LogScanner/cmdScanner/CmdGetTopTimeout.java
@@ -10,28 +10,28 @@ import org.v8LogScanner.rgx.ScanProfile.RgxOpTypes;
 
 public class CmdGetTopTimeout implements CmdCommand {
 
-	@Override
-	public String getTip() {
-		return "";
-	}
+  @Override
+  public String getTip() {
+    return "";
+  }
 
-	@Override
-	public void execute() {
-		
-		V8LogScannerAppl appl = V8LogScannerAppl.instance();
-		
-		appl.profile = new LanScanProfile(RgxOpTypes.CURSOR_OP);
-		appl.clientsManager.localClient().setProfile(appl.profile);
-		
-		// Managed 1C lock
-		RegExp ttimeout = new RegExp(EventTypes.TTIMEOUT);
-		appl.profile.getRgxList().add(ttimeout);
-		
-	// DATABASE DBV8DBEng log
-		RegExp fileTimeout = new RegExp(EventTypes.EXCP);
-		Filter<String> fl = fileTimeout.getFilter(PropTypes.Descr);
-		fl.add("Конфликт блокировок при выполнении транзакции");
-		appl.profile.getRgxList().add(fileTimeout);
-	
-	}
+  @Override
+  public void execute() {
+    
+    V8LogScannerAppl appl = V8LogScannerAppl.instance();
+    
+    appl.profile = new LanScanProfile(RgxOpTypes.CURSOR_OP);
+    appl.clientsManager.localClient().setProfile(appl.profile);
+    
+    // Managed 1C lock
+    RegExp ttimeout = new RegExp(EventTypes.TTIMEOUT);
+    appl.profile.getRgxList().add(ttimeout);
+    
+    // DATABASE DBV8DBEng log
+    RegExp fileTimeout = new RegExp(EventTypes.EXCP);
+    Filter<String> fl = fileTimeout.getFilter(PropTypes.Descr);
+    fl.add("Конфликт блокировок при выполнении транзакции");
+    appl.profile.getRgxList().add(fileTimeout);
+    
+  }
 }
